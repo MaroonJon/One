793f3c440c2656db9facdd00dc4774c786254a7c
MaroonJon <m.jonawaz@hotmail.com>
2024-05-05 22:21:46 +0200

    add contact us and about us pages

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header/Header.jsx';
+import Footer from "../components/Footer/Footer.jsx";
+
+function ContactPage() {
+    return (
+        <div>
+            <Header isHomePage={false} />
+            <main>
+                <h1>Contact</h1>                
+            </main>
+            <Footer />
+        </div>
+
+    );
+}
+
+export default ContactPage;
