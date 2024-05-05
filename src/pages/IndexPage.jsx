26c36b73322ca1aa3f12892ba8e5fb22024b34f5
MaroonJon <m.jonawaz@hotmail.com>
2024-05-05 21:37:48 +0200

    use header and footer for index

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,10 +1,14 @@
-function IndexPage() {
-  return (
-    <div>
-        <h1>index page</h1>
-    </div>
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
-  );
+function IndexPage() {
+    return (
+        <div>
+            <Header isHomePage={true}/>
+            <h1>index page</h1>
+            <Footer/>
+        </div>
+    );
 }
 
 export default IndexPage;
